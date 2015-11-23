9b1184867e44c67f27c1d5ab2d3b76f73255846d
chailuck <chailuck@dtac.co.th>
2015-11-23 12:09:29 +0700

    Learn scope

--- 2_form_and_controller/main.js
+++ 2_form_and_controller/main.js
@@ -1,1 +1,11 @@
+var myApp = angular.module('myApp',[]);
+myApp.controller('FormController', function($scope) {
+	$scope.formData = {
+		name: 'Poppop',
+		email: 'ttt.@test.com'
+	}
 
+	$scope.register = function() {
+		alert("Registered by " + $scope.formData.name + " (" + $scope.formData.email +")")
+	}
+})
