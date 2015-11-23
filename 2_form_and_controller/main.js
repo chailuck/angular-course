9a1592f34b440e360ef1e2ed6c92c46c71b44980
chailuck <chailuck@dtac.co.th>
2015-11-23 12:38:50 +0700

    Three ways to create modules

--- 2_form_and_controller/main.js
+++ 2_form_and_controller/main.js
@@ -1,4 +1,6 @@
+/*
 var myApp = angular.module('myApp',[]);
+
 myApp.controller('FormController', function($scope) {
 	$scope.formData = {
 		name: 'Poppop',
@@ -9,3 +11,36 @@ myApp.controller('FormController', function($scope) {
 		alert("Registered by " + $scope.formData.name + " (" + $scope.formData.email +")")
 	}
 })
+*/
+/*
+angular.module('myApp',[]);
+
+angular.module('myApp')
+	.controller('FormController', function($scope) {
+		$scope.formData = {
+			name: 'Poppop',
+			email: 'ttt.@test.com'
+		}
+
+		$scope.register = function() {
+			alert("Registered by " + $scope.formData.name + " (" + $scope.formData.email +")")
+		}
+	})
+*/
+
+angular.module('myApp',[]);
+
+angular.module('myApp')
+	.controller('FormController', FormController);
+
+function FormController($scope) {
+		$scope.formData = {
+			name: 'Poppop',
+			email: 'ttt.@test.com'
+		}
+
+		$scope.register = function() {
+			alert("Registered by " + $scope.formData.name + " (" + $scope.formData.email +")")
+		}
+}
+
