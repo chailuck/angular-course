bc081a5b5f53baa9f8567e04d36b7510f91ecfd4
chailuck <chailuck@dtac.co.th>
2015-11-24 14:47:08 +0700

    Add broadcast

---
+++ 11_broadcast/main.js
@@ -0,0 +1,45 @@
+var app = angular.module('myApp', []);
+
+app.controller('ProfileListController', function ($scope,$rootScope) {
+  
+  $scope.profiles = [
+    {
+      name: "Twin PanichSombat",
+      birthdate: new Date(1975, 3, 3),
+    },
+    {
+      name: "Salah Chalermthai",
+      birthdate: new Date(1981, 2, 23),
+    },
+    {
+      name: "Steven Gerrad",
+      birthdate: new Date(1980, 4, 30),
+    },
+    {
+      name: "Michael Owen",
+      birthdate: new Date(1979, 11, 14),
+    },
+    {
+      name: "Luis Suárez",
+      birthdate: new Date(1987, 0, 24),
+    },
+  ];
+
+  $scope.postSubmit = function() {
+
+  }
+
+	$scope.edit = function(selectedProfile){
+		
+    $rootScope.$broadcast('editedProfile', selectedProfile);
+  };
+
+});
+
+app.controller('ProfileDetailController', function ($scope) {
+  
+  $scope.$on('editedProfile', function (event, data) {
+    $scope.editProfile = data;
+  });
+});
+
